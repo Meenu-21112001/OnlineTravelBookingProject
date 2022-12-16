a1946ebf33880e12eedac1dcf97afef7673a11df
Meenu-21112001 <114347759+Meenu-21112001@users.noreply.github.com>
2022-12-16 19:44:26 +0530

    Update UserDefaultResponseDto.java

--- OnlineTravel_BookingHotel/src/main/java/com/travel/dto/UserDefaultResponseDto.java
+++ OnlineTravel_BookingHotel/src/main/java/com/travel/dto/UserDefaultResponseDto.java
@@ -4,37 +4,32 @@ import org.springframework.stereotype.Component;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 @Component
-public class UserDefaultResponseDto implements MyDto
-{
+public class UserDefaultResponseDto implements MyDto {
 	private String hotelname;
 	private int hotelId;
-	
-	
+
 	public UserDefaultResponseDto() {
 		super();
 	}
-	public UserDefaultResponseDto(String hotelName2, int hotelId2)
-	{
-		// TODO Auto-generated constructor stub
-	}
+
 	public String getHotelname() {
 		return hotelname;
 	}
+
 	public void setHotelname(String hotelname) {
 		this.hotelname = hotelname;
 	}
+
 	public int getHotelId() {
 		return hotelId;
 	}
+
 	public void setHotelId(int hotelId) {
 		this.hotelId = hotelId;
 	}
-	
-}
 
+}
